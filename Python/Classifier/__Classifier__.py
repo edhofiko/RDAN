31f4ddb5727d3d42556daeb768120525ec851df1
edho08 <edho08@gmail.com>
2020-01-13 13:01:36 +0700

    add DAN-GRU
    - add DAN-GRU classifier

--- Python/Classifier/__Classifier__.py
+++ Python/Classifier/__Classifier__.py
@@ -1,4 +1,4 @@
-from NeuralNetwork.Layer import Dense, Convolve1d, GRU, Tanh, Relu, CrossEntropySoftmax, RNN, Softmax, biGRU
+from NeuralNetwork.Layer import Dense, Convolve1d, GRU, Tanh, Relu, CrossEntropySoftmax, RNN, Softmax, biGRU, LeakyRelu
 from NeuralNetwork.Loss import CategoricalCrossEntropySoftmax, SSE
 from NeuralNetwork.Model import Sequential
 from NeuralNetwork.Optimizer import AdaMax
@@ -111,4 +111,53 @@ class HierGRUTextClassifier:
 	def load(filedir):
 		return dill.load(open(filedir, "rb"))
 		
+class DANGRUTextClassifier:
+	def __init__(self, vectorizer, word_vector_size, num_of_class, hidden_size=15):
+		self.vectorizer = vectorizer
+		self.layer5 = GRU((word_vector_size, 2*word_vector_size), s2v=True)
+		self.layer6 = Dense((2*word_vector_size, hidden_size))
+		self.layer7 = LeakyRelu()
+		self.layer10 = Dense((hidden_size, num_of_class))
+		self.layer11 = CrossEntropySoftmax()
+		self.model2 = Sequential([self.layer5, self.layer6, self.layer7, self.layer10, self.layer11])
+		self.Loss = CategoricalCrossEntropySoftmax()
+
+	def transform_input(self, doc):
+		transformed = np.array([self.vectorizer.batch_vectorize(sentence.split()).mean(axis=0) for sentence in doc.split("--")])
+		return transformed
 		
+
+	def forward(self, x):
+		return self.model2.forward(self.model1.batch_forward(x))
+
+	def train(self, x, y):
+		p = self.model2.forward(x)
+		derr = self.Loss.dloss(y, p)
+		derr = self.model2.backward(x, derr)
+		return self.Loss.loss(y, p) / len(p), np.argmax(p), np.argmax(y)
+
+	def fit(self, corpus, y, max_epoch=50000000, min_error=1e-1, verbose=True):
+		epoch = 0
+		loss = float("inf")	
+		corpus = [self.transform_input(doc) for doc in corpus]
+		train_data = list(zip(corpus, y))
+		while (epoch < max_epoch and loss > min_error):
+			loss = 0
+			acc = 0
+			for index, (x, t) in enumerate(train_data):
+				l, p, t =  self.train(x, t)
+				loss += l
+				acc += 1 if p == t else 0
+				print(epoch, str(index+1)+"/"+str(len(corpus)), loss/(index+1), t,p,acc/(index+1), end='\r')
+			#self.save("CNN-GRU_Cassifier_temp.pkl")
+			print()
+			epoch+=1
+		self.save("data/CNN-GRU_Cassifier.pkl")
+			
+	def save(self, filedir):
+		dill.dump(self, open(filedir, "wb"))
+
+	def load(filedir):
+		return dill.load(open(filedir, "rb"))
+		
+				
--- Python/classifer_test_grugru.py
+++ Python/classifer_test_grugru.py
@@ -1,4 +1,4 @@
-from Classifier import HierGRUTextClassifier
+from Classifier import DANGRUTextClassifier
 from Vectorizer import word2vec
 from NeuralNetwork.Layer import OneHotter
 import random
@@ -9,7 +9,7 @@ import numpy as np
 vectorizer = word2vec.load("data/word2vec_skipgram.pkl")
 
 data = pd.read_csv("data/data_berita_prep.csv")
-gps = data.groupby(["kanal"]).head(100).reset_index(drop=True)
+gps = data.groupby(["kanal"]).head(5).reset_index(drop=True)
 
 #l = gps["body"].map(lambda x : max([len(x.split("--"))]))
 
@@ -21,6 +21,6 @@ Y = onehotter(Y)
 
 del data
 
-classifier = HierGRUTextClassifier(vectorizer, 50, 14)
+classifier = DANGRUTextClassifier(vectorizer, 50, 14)
 classifier.fit(X[:], Y[:])
 
